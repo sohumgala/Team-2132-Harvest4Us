532d06d5f7efce7efbec38ce0d45b43200a3490f
Sohum Gala <sohum.gala@gmail.com>
2022-04-15 12:45:31 -0400

    add all links to cards

--- EducationalResources-Demo/app/src/main/java/com/example/educationalresources_demo/FirstFragment.kt
+++ EducationalResources-Demo/app/src/main/java/com/example/educationalresources_demo/FirstFragment.kt
@@ -41,6 +41,66 @@ class FirstFragment : Fragment() {
             startActivity(browserIntent)
         })
 
+        val article2: CardView = view.findViewById(R.id.article_2)
+        article2.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.localharvest.org"))
+            startActivity(browserIntent)
+        })
+
+        val article3: CardView = view.findViewById(R.id.article_3)
+        article3.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.fns.usda.gov/snap/supplemental-nutrition-assistance-program"))
+            startActivity(browserIntent)
+        })
+
+        val article4: CardView = view.findViewById(R.id.article_4)
+        article4.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.fns.usda.gov/wic"))
+            startActivity(browserIntent)
+        })
+
+        val article5: CardView = view.findViewById(R.id.article_5)
+        article5.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.fns.usda.gov/sfmnp/senior-farmers-market-nutrition-program"))
+            startActivity(browserIntent)
+        })
+
+        val article6: CardView = view.findViewById(R.id.article_6)
+        article6.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://findfoodsupport.withgoogle.com"))
+            startActivity(browserIntent)
+        })
+
+        val article7: CardView = view.findViewById(R.id.article_7)
+        article7.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.nutrition.gov/topics/basic-nutrition/healthy-eating"))
+            startActivity(browserIntent)
+        })
+
+        val article8: CardView = view.findViewById(R.id.article_8)
+        article8.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.heart.org/en/healthy-living/healthy-eating"))
+            startActivity(browserIntent)
+        })
+
+        val article9: CardView = view.findViewById(R.id.article_9)
+        article9.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://freedge.org/locations/"))
+            startActivity(browserIntent)
+        })
+
+        val article10: CardView = view.findViewById(R.id.article_10)
+        article10.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://blackchurchfoodsecurity.net/become-a-member/"))
+            startActivity(browserIntent)
+        })
+
+        val article11: CardView = view.findViewById(R.id.article_11)
+        article11.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://blackfarmersnetwork.com/category/education/"))
+            startActivity(browserIntent)
+        })
+        
     }
 
     override fun onDestroyView() {
