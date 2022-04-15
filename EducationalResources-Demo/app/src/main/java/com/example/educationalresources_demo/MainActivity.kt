e64955e67a3f5410b4c9d765e7e101b17213505a
Sohum Gala <sohum.gala@gmail.com>
2022-04-15 12:56:29 -0400

    delete top toolbar and floating mail icons

--- EducationalResources-Demo/app/src/main/java/com/example/educationalresources_demo/MainActivity.kt
+++ EducationalResources-Demo/app/src/main/java/com/example/educationalresources_demo/MainActivity.kt
@@ -22,16 +22,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
