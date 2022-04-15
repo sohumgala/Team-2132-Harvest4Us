6b5fff763414eb86789185045f8602ec1485f3a3
Sohum Gala <sohum.gala@gmail.com>
2022-04-15 12:34:07 -0400

    add link to article 1

--- EducationalResources-Demo/app/src/main/java/com/example/educationalresources_demo/FirstFragment.kt
+++ EducationalResources-Demo/app/src/main/java/com/example/educationalresources_demo/FirstFragment.kt
@@ -1,12 +1,15 @@
 package com.example.educationalresources_demo
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import com.example.educationalresources_demo.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -32,9 +35,12 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        binding.buttonFirst.setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        }
+        val article1: CardView = view.findViewById(R.id.article_1)
+        article1.setOnClickListener(View.OnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ams.usda.gov/local-food-directories/farmersmarkets"))
+            startActivity(browserIntent)
+        })
+
     }
 
     override fun onDestroyView() {
