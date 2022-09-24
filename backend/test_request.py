5d20bd721ba5c3e0abb442144ffa3925f44d85e7
Sohum Gala <sohum.gala@gmail.com>
2022-09-24 15:16:41 -0400

    change login test requests to HTTP POST

--- backend/test_request.py
+++ backend/test_request.py
@@ -3,8 +3,8 @@ import requests
 resp = requests.get("http://127.0.0.1:5000/get_users/")
 print(resp.json())
 
-resp1 = requests.get("http://127.0.0.1:5000/login/", json = {"username": "fff", "password": "ggg"})
+resp1 = requests.post("http://127.0.0.1:5000/login/", json = {"username": "fff", "password": "ggg"})
 print(resp1.status_code) # should be 200
 
-resp2 = requests.get("http://127.0.0.1:5000/login/", json = {"username": "fff", "password": "gggg"})
+resp2 = requests.post("http://127.0.0.1:5000/login/", json = {"username": "fff", "password": "gggg"})
 print(resp2.status_code) # should be 401
