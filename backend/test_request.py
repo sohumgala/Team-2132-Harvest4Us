95b1889e4567eeb520eaf61b4e39d3a45b0f5286
Sohum Gala <sohum.gala@gmail.com>
2022-10-12 11:33:59 -0400

    add test_request http code prints

--- backend/test_request.py
+++ backend/test_request.py
@@ -15,8 +15,10 @@ print(resp3.status_code) # should be 401
 resp4 = requests.post("http://127.0.0.1:5000/register/", json = {"username": "ggg", "first_name": "dummy_first", "last_name": "dummy_last", "password": "dummy_pass"})
 print(resp4.status_code) # should be 200 (only for the first time it's run, then 401)
 
-resp = requests.get("http://127.0.0.1:5000/get_all_produce/")
-print(resp.json())
+resp5 = requests.get("http://127.0.0.1:5000/get_all_produce/")
+print(resp5.json())
+print(resp5.status_code)
 
-resp = requests.get("http://127.0.0.1:5000/get_all_farms/")
-print(resp.json())
+resp6 = requests.get("http://127.0.0.1:5000/get_all_farms/")
+print(resp6.json())
+print(resp6.status_code)
