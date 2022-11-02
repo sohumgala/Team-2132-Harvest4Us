15663f3cbaca6fb45c41d90c22d204c241aa30e1
Sohum Gala <sohum.gala@gmail.com>
2022-11-02 16:59:41 -0400

    add get_produce_by_producer and change_quantity backend endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response, jsonify
+from datetime import datetime
 import json
 import mysql.connector
 
@@ -49,6 +50,12 @@ def get_all_produce():
     produce = select("select product_id, business_name, produceType, produceCategory, unit, usdaGrade, active, availableQuantity, dateEdited, organic, price, 0 from producer_users inner join inventory on producer_users.username = inventory.producer where active = 1")
     return jsonify(produce), 200
 
+@app.route("/get_produce_by_producer/", methods=["POST"])
+def get_produce_by_producer():
+    data = request.get_json()
+    produce = select("select product_id, business_name, produceType, produceCategory, unit, usdaGrade, active, availableQuantity, dateEdited, organic, price, 0 from producer_users inner join inventory on producer_users.username = inventory.producer where active = 1 and producer_users.business_name = %s", (data["business_name"],))
+    return jsonify(produce), 200
+
 @app.route("/get_all_farms/", methods=["GET"])
 def get_all_farms():
     farms = select("select business_name, description, city, st from producer_users")
@@ -59,6 +66,26 @@ def get_cart():
     data = request.get_json()
     cart = select("select active_inventory.product_id, active_inventory.business_name, produceType, produceCategory, active_inventory.unit, active_inventory.usdaGrade, active_inventory.active, active_inventory.availableQuantity, active_inventory.dateEdited, active_inventory.organic, active_inventory.price, carts.quantity, round(price * carts.quantity, 2) as 'itemPrice' from (select * from producer_users inner join inventory on producer_users.username = inventory.producer where active = 1) as active_inventory inner join carts on (carts.producer, carts.product_id) = (active_inventory.username, active_inventory.product_id) where carts.consumer = %s", (data["username"],))
     return jsonify(cart), 200
+
+@app.route("/change_quantity/", methods=["POST"])
+def change_quantity():
+    data = request.get_json()
+    curr_quantity = select("select quantity from carts where producer = (select username from producer_users where business_name = %s) and product_id = %s and consumer = %s", (data["business_name"], data["product_id"], data["username"]))
+    if len(curr_quantity) != 0 and data["new_quantity"] == 0:
+        modify("delete from carts where producer = (select username from producer_users where business_name = %s) and product_id = %s and consumer = %s", (data["business_name"], data["product_id"], data["username"]))
+        return Response("{}", status = 200)
+
+    avail_quantity = select("select availableQuantity from inventory where producer = (select username from producer_users where business_name = %s) and product_id = %s", (data["business_name"], data["product_id"]))
+    if len(curr_quantity) != 0 and len(avail_quantity) != 0 and data["new_quantity"] <= avail_quantity[0][0] and data["new_quantity"] > 0:
+        modify("update carts set quantity = %s where producer = (select username from producer_users where business_name = %s) and product_id = %s and consumer = %s", (data["new_quantity"], data["business_name"], data["product_id"], data["username"]))
+        return Response("{}", status = 200)
+
+    if len(curr_quantity) == 0 and len(avail_quantity) != 0 and data["new_quantity"] <= avail_quantity[0][0] and data["new_quantity"] > 0:
+        today = datetime.today().strftime('%Y-%m-%d')
+        modify("insert into carts values ((select username from producer_users where business_name = %s), %s, %s, %s, %s)", (data["business_name"], data["product_id"], data["username"], today, data["new_quantity"]))
+        return Response("{}", status = 200)
+
+    return Response("{}", status = 403)
 
 if __name__ == "main":
     with open("db_auth.json", "r") as f:
--- backend/test_request.py
+++ backend/test_request.py
@@ -26,3 +26,44 @@ print(resp6.status_code)
 resp7 = requests.post("http://127.0.0.1:5000/get_cart/", json = {"username": "fff"})
 print(resp7.json())
 print(resp7.status_code)
+
+resp8 = requests.post("http://127.0.0.1:5000/get_produce_by_producer/", json = {"business_name": "A Business"})
+print(resp8.json())
+print(resp8.status_code)
+
+# Tests for change_quantity - look at output of get_cart to see the changes
+# Note this set of operations takes the database back to its original state
+# delete item
+resp9 = requests.post("http://127.0.0.1:5000/change_quantity/", json = {"business_name": "A Business", "product_id": 1, "username": "fff", "new_quantity": 0})
+print(resp9.status_code)
+
+resp_cart = requests.post("http://127.0.0.1:5000/get_cart/", json = {"username": "fff"})
+print(resp_cart.json())
+
+# re add item
+resp10 = requests.post("http://127.0.0.1:5000/change_quantity/", json = {"business_name": "A Business", "product_id": 1, "username": "fff", "new_quantity": 3})
+print(resp10.status_code)
+
+resp_cart = requests.post("http://127.0.0.1:5000/get_cart/", json = {"username": "fff"})
+print(resp_cart.json())
+
+# increase quantity
+resp11 = requests.post("http://127.0.0.1:5000/change_quantity/", json = {"business_name": "A Business", "product_id": 1, "username": "fff", "new_quantity": 4})
+print(resp11.status_code)
+
+resp_cart = requests.post("http://127.0.0.1:5000/get_cart/", json = {"username": "fff"})
+print(resp_cart.json())
+
+# decrease quantity
+resp12 = requests.post("http://127.0.0.1:5000/change_quantity/", json = {"business_name": "A Business", "product_id": 1, "username": "fff", "new_quantity": 3})
+print(resp12.status_code)
+
+resp_cart = requests.post("http://127.0.0.1:5000/get_cart/", json = {"username": "fff"})
+print(resp_cart.json())
+
+# add more than available (nop)
+resp13 = requests.post("http://127.0.0.1:5000/change_quantity/", json = {"business_name": "A Business", "product_id": 1, "username": "fff", "new_quantity": 50})
+print(resp13.status_code)
+
+resp_cart = requests.post("http://127.0.0.1:5000/get_cart/", json = {"username": "fff"})
+print(resp_cart.json())
