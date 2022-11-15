5655791c92c9c095f46b3c07e499cf46497f9aed
oi6 <oi6@gatech.edu>
2022-11-15 12:34:58 -0500

    cart button added to all pages

--- Harvest4usMOBILE/app/src/main/java/com/amm/harvest4us/FarmsActivity.kt
+++ Harvest4usMOBILE/app/src/main/java/com/amm/harvest4us/FarmsActivity.kt
@@ -7,12 +7,14 @@ import android.os.Looper
 import android.os.Message
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.amm.harvest4us.items.ProduceItem
 import com.amm.harvest4us.items.ProducerItem
+import com.amm.harvest4us.items.jsonArrToProduceItemList
 import com.amm.harvest4us.items.jsonArrToProducerItem
 import okhttp3.*
 import org.json.JSONArray
@@ -110,6 +112,74 @@ class FarmsActivity : AppCompatActivity(), CellClickListenerFarms {
         val recyclerview = findViewById<RecyclerView>(R.id.farmView)
         recyclerview.adapter = adapter
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+
+        // Initializes the search bar
+        val search = menu?.findItem(R.id.nav_search)
+        val searchView = search?.actionView as SearchView
+        searchView.queryHint = "Search something!"
+
+        val cartB = menu?.findItem(R.id.nav_cart)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // if there is something in the search bar,
+                if (query != null) {
+                    setContentView(R.layout.activity_marketplace)
+                    val recyclerview = findViewById<RecyclerView>(R.id.lv_listView)
+                    // this creates a vertical layout Manager
+                    recyclerview.layoutManager = LinearLayoutManager(applicationContext)
+                    // Display the items pertaining to the search string
+//                    updateProduceList(query)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    setContentView(R.layout.activity_marketplace)
+                    // getting the recyclerview by its id
+                    val recyclerview = findViewById<RecyclerView>(R.id.lv_listView)
+                    // this creates a vertical layout Manager
+                    recyclerview.layoutManager = LinearLayoutManager(applicationContext)
+//                    updateProduceList(newText)
+                }
+                return true
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // handle cart activities
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id: Int = item.getItemId()
+        if (id == R.id.nav_cart) {
+            val intent = Intent(this, CartActivity::class.java)
+            intent.putExtra("username", username)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Make a backend call to update the produce list using the query searchString.
+     * ProduceList will be updated asynchronously when the backend call returns.
+     */
+//    fun updateProduceList(searchString: String) {
+//        val responseHandler = object : Handler(Looper.getMainLooper()) {
+//            override fun handleMessage(msg: Message) {
+//                if (msg.what == -1) return // backend call failed
+//                produceList = jsonArrToProduceItemList(JSONArray(msg.obj as String))
+//                updateProduceListView()
+//            }
+//        }
+//        backend.getAllProduce(responseHandler)
+//    }
+
+
 
 
 }
